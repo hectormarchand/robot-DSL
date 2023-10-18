0d70e8f3341550057d0c5eef01b0c5d941a59c1c
Hector Marchand <hector.marchand@orange.fr>
2023-10-18 15:03:47 +0200

    Initial commit

---
+++ src/language/robot-language-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { RobotLanguageAstType, Person } from './generated/ast.js';
+import type { RobotLanguageServices } from './robot-language-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: RobotLanguageServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.RobotLanguageValidator;
+    const checks: ValidationChecks<RobotLanguageAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class RobotLanguageValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
