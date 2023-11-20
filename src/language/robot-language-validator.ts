fcdbfadb0a240724d134e534c3cdf9250eab5b3e
Hector Marchand <hector.marchand@orange.fr>
2023-11-20 09:57:48 +0100

    continue grammar

--- src/language/robot-language-validator.ts
+++ src/language/robot-language-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { RobotLanguageAstType, Person } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+import type { RobotLanguageAstType } from './generated/ast.js';
 import type { RobotLanguageServices } from './robot-language-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: RobotLanguageServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.RobotLanguageValidator;
     const checks: ValidationChecks<RobotLanguageAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+       // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: RobotLanguageServices) {
  */
 export class RobotLanguageValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
